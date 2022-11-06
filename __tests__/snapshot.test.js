2983598ae7024a8ce3392548aed7c71ef4a21e56
Indrajith C <indrajith.chandran@mozanta.com>
2022-11-06 19:19:30 +0530

    snapshot test added

---
+++ __tests__/snapshot.test.js
@@ -0,0 +1,9 @@
+// __tests__/snapshot.js
+
+import { render } from "@testing-library/react";
+import Home from "../pages/index";
+
+it("renders homepage unchanged", () => {
+  const { container } = render(<Home />);
+  expect(container).toMatchSnapshot();
+});
