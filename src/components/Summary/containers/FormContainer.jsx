ff44d3eca2f03c55713bab3f6d460596714c380e
Indrajith C <indrajith.chandran@mozanta.com>
2022-11-06 23:48:23 +0530

    src folder added

---
+++ src/components/Summary/containers/FormContainer.jsx
@@ -0,0 +1,21 @@
+/*
+ * Copyright(c) 2022 
+ * 
+ * All rights reserved.
+ * 
+ * @author Indrajith C
+ */
+
+// import propTypes from "prop-types";
+
+const FormContainer = props => {
+
+  console.log({ props });
+  return (<div />)
+}
+
+FormContainer.propTypes = {
+
+}
+
+export default FormContainer;
