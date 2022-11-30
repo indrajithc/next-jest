2d65c5dbd9755a8ed57a29ac0569ea94e16f797c
Indrajith C <indrajith.chandran@mozanta.com>
2022-11-30 15:47:04 +0530

    setup file added

--- jest.config.js
+++ jest.config.js
@@ -31,7 +31,7 @@ module.exports = {
     "^@mocks/(.*)$": "<rootDir>/__mocks__/$1",
   },
   // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
   testEnvironment: "jsdom",
   transform: {
