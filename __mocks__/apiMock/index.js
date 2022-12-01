57dd1b7d43ab0601d4adcde00449192194d74059
Indrajith C <indrajith.chandran@mozanta.com>
2022-12-01 18:59:08 +0530

    moved check into function

--- __mocks__/apiMock/index.js
+++ __mocks__/apiMock/index.js
@@ -1,7 +1,5 @@
-const IS_BROWSER = typeof window !== "undefined";
-
 export const setupMocks = async () => {
-  if (IS_BROWSER) {
+  if (typeof window !== "undefined") {
     const { mswWorker } = await import("./mswWorker");
     mswWorker.start();
   } else {
