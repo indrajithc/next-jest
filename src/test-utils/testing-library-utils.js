5575e07012a0333b5aa7398214d786bbf3fa6c6d
Indrajith C <indrajith.chandran@mozanta.com>
2022-12-06 10:16:17 +0530

    testing utils added

---
+++ src/test-utils/testing-library-utils.js
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react";
+import { OrderDetailsProvider } from "@app/contexts/OrderDetails";
+
+const renderWithContext = (ui, options) =>
+  render(ui, {
+    wrapper: OrderDetailsProvider,
+    ...options,
+  });
+
+// re-export everything
+
+export * from "@testing-library/react";
+
+// override render method
+export { renderWithContext as render };
