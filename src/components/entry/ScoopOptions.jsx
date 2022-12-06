83bddd1b1042b0c8304cc81867d1d7ec3f5ae85b
Indrajith C <indrajith.chandran@mozanta.com>
2022-12-06 10:17:30 +0530

    spin buttons added

--- src/components/entry/ScoopOptions.jsx
+++ src/components/entry/ScoopOptions.jsx
@@ -7,13 +7,25 @@
  */
 
 import PropTypes from "prop-types";
+import { useOrderDetails } from "@app/contexts/OrderDetails";
 
 function ScoopOptionsComponent(props) {
   const { name, imagePath } = props;
 
+  const { updateItemCount } = useOrderDetails();
+  const handleChange = (e) => updateItemCount(name, e.target.value, "scoops");
+
   return (
     <div>
       <img src={imagePath} alt={`${name} scoop`} />
+      <input
+        id={`counter${name}`}
+        type="number"
+        role="spinbutton"
+        onChange={handleChange}
+        defaultValue={0}
+      />
+      <label htmlFor={`counter${name}`}>{name}</label>
     </div>
   );
 }
