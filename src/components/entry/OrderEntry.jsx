a3eafa76cfa7432d0571d8a88f17f68e68a9dd70
Indrajith C <indrajith.chandran@mozanta.com>
2022-12-09 17:03:26 +0530

    grand total content added

--- src/components/entry/OrderEntry.jsx
+++ src/components/entry/OrderEntry.jsx
@@ -1,10 +1,16 @@
 import Options from "@components/entry/Options";
+import { formatCurrency } from "@app/utilities/index";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
 function OrderEntry() {
+  const { totals } = useOrderDetails();
+
   return (
     <div>
+      <h1>Design your Sundae!</h1>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
+      <h2>Grand total: {formatCurrency(totals.scoops + totals.toppings)}</h2>
     </div>
   );
 }
