8b6510a3366cd7ec8cd2d715622d3cafd5007065
Indrajith C <indrajith.chandran@mozanta.com>
2022-12-11 19:16:02 +0530

    index page added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,36 @@
+import { useState } from "react";
+
+import OrderEntry from "@app/components/entry/OrderEntry";
+import OrderSummary from "@app/components/entry/OrderSummary";
+import OrderConfirmation from "@app/components/confirmation/OrderConfirmation";
+
+import { OrderDetailsProvider } from "@app/contexts/OrderDetails";
+
 export default function Home() {
+  // orderPhase needs to be 'inProgress', 'review' or 'completed'
+  const [orderPhase, setOrderPhase] = useState("inProgress");
+
+  let Component = OrderEntry; // default to order page
+  switch (orderPhase) {
+    case "inProgress":
+      Component = OrderEntry;
+      break;
+    case "review":
+      Component = OrderSummary;
+      break;
+    case "completed":
+      Component = OrderConfirmation;
+      break;
+    default:
+  }
+
   return (
     <div>
       <main>
         <h1 role="presentation">Welcome</h1>
+        <OrderDetailsProvider>
+          <Component setOrderPhase={setOrderPhase} />
+        </OrderDetailsProvider>
       </main>
     </div>
   );
