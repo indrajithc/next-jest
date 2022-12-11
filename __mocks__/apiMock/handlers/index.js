779b1bfa3f438aefe9dcd140feb3bf51ddc62b8b
Indrajith C <indrajith.chandran@mozanta.com>
2022-12-11 16:55:00 +0530

    order handlers added

--- __mocks__/apiMock/handlers/index.js
+++ __mocks__/apiMock/handlers/index.js
@@ -1,4 +1,9 @@
 import { scoopsHandlers } from "./api/scoops";
 import { toppingsHandlers } from "./api/toppings";
+import { orderHandlers } from "./api/order";
 
-export const handlers = [...scoopsHandlers, ...toppingsHandlers];
+export const handlers = [
+  ...scoopsHandlers,
+  ...toppingsHandlers,
+  ...orderHandlers,
+];
